075d3178a35cbb81a02a1783e0d8d26fff752ac1
Martine R Karlsen <martinekar08@gmail.com>
2025-09-11 15:20:57 +0200

    Create post form submission with validation
    - get the Create form by Id
    - prevent the form from submitting when refreshing
    - get accessToken, userName and apiKey from localStorage
    - check to see if there is an accessToken, othervise direct the user to login
    - get the input values from (title, body, media, alt)
    - error false by default unless any if statements are true
    - input validation, no input fields can be empty
    - create a new object called post containing: title, media (including alt), body.
    - Send the POST to API to create a new post
    - alert if post created successfully
    - reset form
    - direct user to landing page
    - show alert if error occurs when trying to create post

---
+++ src/js/post/create.js
@@ -0,0 +1,103 @@
+import {
+  showError,
+  clearError,
+  Count,
+  API_BASE_URL,
+  API_ENDPOINTS,
+  attachInputListeners,
+} from "../utils.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Count();
+});
+
+const createPostForm = document.getElementById("create-form");
+
+createPostForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const accessToken = localStorage.getItem("accessToken");
+  const userName = localStorage.getItem("userName");
+  const apiKey = localStorage.getItem("apiKey");
+
+  if (!accessToken) {
+    alert("You must be logged in to create a post.");
+    window.location.href = "../../auth/login/index.html";
+    return;
+  }
+
+  const title = document.getElementById("title").value;
+  const body = document.getElementById("body").value;
+  const media = {
+    url: document.getElementById("media").value,
+    alt: document.getElementById("alt").value,
+  };
+
+  let hasError = false;
+
+  if (!title.trim()) {
+    showError("title", "Title cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("title");
+  }
+
+  if (!body.trim()) {
+    showError("body", "Post caption cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("body");
+  }
+
+  if (!media.url.trim() || !media.url.trim().startsWith("http")) {
+    showError(
+      "media",
+      "Please enter a valid image url that starts with http or https."
+    );
+    hasError = true;
+  } else {
+    clearError("media");
+  }
+
+  if (!media.alt.trim()) {
+    showError("alt", "ALT text cannot be empty.");
+    hasError = true;
+  } else {
+    clearError("alt");
+  }
+
+  if (hasError) return;
+
+  const post = { title, media, body };
+
+  try {
+    const response = await fetch(
+      `${API_BASE_URL}${API_ENDPOINTS.SOCIAL.POSTS}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+          "X-Noroff-API-Key": apiKey,
+        },
+        body: JSON.stringify(post),
+      }
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage =
+        data.errors?.[0]?.message || `HTTP ${response.status}`;
+      throw new Error(errorMessage);
+    }
+
+    alert("Post created!");
+    createPostForm.reset();
+    window.location.href = "/index.html";
+  } catch (error) {
+    const errorMessage = error.message || "An undexpected error occured.";
+    alert("Something went wrong while creating the post: ", errorMessage);
+  }
+});
+
+attachInputListeners(["#title", "#media", "#body", "#alt"]);
